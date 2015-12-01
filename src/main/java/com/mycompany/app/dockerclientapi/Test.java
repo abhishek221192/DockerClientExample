eff78801cc9151aa0c15794eadeaa4db3ebd05d9
Abhishek Singh <abhishek221192@gmail.com>
2015-12-01 14:31:05 +0530

    =other lib

--- src/main/java/com/mycompany/app/dockerclientapi/Test.java
+++ src/main/java/com/mycompany/app/dockerclientapi/Test.java
@@ -6,21 +6,12 @@
 package com.mycompany.app.dockerclientapi;
 
 import com.editor.dockerrunner.util.Connection;
-import static com.fasterxml.jackson.databind.jsonFormatVisitors.JsonValueFormat.URI;
-import com.spotify.docker.client.DefaultDockerClient;
-import com.spotify.docker.client.DockerClient;
-import com.spotify.docker.client.DockerException;
-import com.spotify.docker.client.LogStream;
-import com.spotify.docker.client.messages.AuthConfig;
-import com.spotify.docker.client.messages.ContainerConfig;
-import com.spotify.docker.client.messages.ContainerCreation;
-import com.spotify.docker.client.messages.ContainerInfo;
-import com.spotify.docker.client.messages.HostConfig;
-import com.spotify.docker.client.messages.PortBinding;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import com.github.dockerjava.api.DockerClient;
+import com.github.dockerjava.api.DockerException;
+import com.github.dockerjava.api.command.CreateContainerResponse;
+import com.github.dockerjava.api.model.Link;
+import com.github.dockerjava.core.DockerClientBuilder;
+import com.github.dockerjava.core.DockerClientConfig;
 
 /**
  *
@@ -30,21 +21,24 @@ public class Test {
 
     public static void main(String[] args) throws DockerException, InterruptedException {
 
-        DockerClient docker = DefaultDockerClient.builder()
-                .uri("https://" + Connection.getAddress())
+        DockerClientConfig config = DockerClientConfig.createDefaultConfigBuilder()
+                .withUri("http://" + Connection.getAddress())
                 .build();
-
-        ContainerConfig containerConfig = ContainerConfig.builder()
-                .image("mongo")
-                //.cmd("sh","-c","'exec mongo \"$MONGO_PORT_27017_TCP_ADDR:$MONGO_PORT_27017_TCP_PORT/test\"'")
-                .attachStdin(Boolean.TRUE)
-                .attachStdout(Boolean.TRUE)
-                .attachStderr(Boolean.TRUE)
-                .build();
-
-        ContainerCreation creation = docker.createContainer(containerConfig);
-        String id = creation.id();
+        DockerClient docker = DockerClientBuilder.getInstance(config).build();
         
+        Link link=new Link("mongo-demo","mongo");
+        
+        CreateContainerResponse createContainer=docker.createContainerCmd("mongo")
+                .withAttachStdin(true)
+                .withAttachStdout(true)
+                .withAttachStderr(true)
+                .withCmd("sh -c 'exec mongo \"$MONGO_PORT_27017_TCP_ADDR:$MONGO_PORT_27017_TCP_PORT/test\"'")
+                .withLinks(link)
+                .exec();
+        
+        String id=createContainer.getId();
+
+
         System.out.println(id);
 
     }
